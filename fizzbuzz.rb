1b149e136e85f07c52d1a6d6619c78f4fd096c8b
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-09 07:44:24 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 
     puts  "Fizz" 
-  elseif int % 5 
+  elsif int % 5 
     puts "Buzz"
-  elseif int % 3 && int % 5
+  elsif int % 3 && int % 5
     puts "FizzBuzz"
   else 
     puts "nil"
